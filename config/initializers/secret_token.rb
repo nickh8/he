b3ce926c69a96ac988e0474c4041539ea8287b11
Ubuntu 12.04.2 <nickh8@ubuntu.(none)>
2013-07-05 02:19:02 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+He::Application.config.secret_key_base = '08da6b47ec254a8db36cb8e38caf99ed704dcc0a4102224b92d655d189e80de90f431a234563ea5cd628b6bfc676170402bcf05bfafc4a32cb6de9a4582d425d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+He::Application.config.session_store :cookie_store, key: '_he_session'
